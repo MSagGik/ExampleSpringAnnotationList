9b6c3263949ea2c5f269c13bed4069ab460c4bfd
MSagGik <108148690+MSagGik@users.noreply.github.com>
2022-08-09 14:40:21 +0300

    Update TestSpring.java

--- src/main/java/ru/msaggik/spring/TestSpring.java
+++ src/main/java/ru/msaggik/spring/TestSpring.java
@@ -1,1 +1,19 @@
-package ru.msaggik.spring;import org.springframework.context.support.ClassPathXmlApplicationContext;public class TestSpring {    public static void main(String[] args) {        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(                "applicationContext.xml"        );        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);        musicPlayer.playMusic(MusicEnum.CLASSICAL);        musicPlayer.playMusic(MusicEnum.TECHNO);        musicPlayer.playMusic(MusicEnum.ROCK);        context.close();    }}
+package ru.msaggik.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        musicPlayer.playMusic(MusicEnum.CLASSICAL);
+        musicPlayer.playMusic(MusicEnum.TECHNO);
+        musicPlayer.playMusic(MusicEnum.ROCK); 
+
+        context.close();
+    }
+}
